1976508dd84b62e312668f827a8b3d61f0fbc697
LaiYoung <laiyoung@yeah.net>
2016-05-10 15:45:52 +0800

    iWatch
    更新UI界面,新增iWatch端发送消息到iOS端

--- IWatch_TableView/IWatch_TableView WatchKit Extension/InterfaceController.m
+++ IWatch_TableView/IWatch_TableView WatchKit Extension/InterfaceController.m
@@ -6,6 +6,8 @@
 //  Copyright © 2016年 LaiYoung. All rights reserved.
 //
 
+/** https://github.com/CYBoys/iWatch */
+
 #import "InterfaceController.h"
 #import <WatchConnectivity/WatchConnectivity.h>
 #import "HiSchool.h"
@@ -14,10 +16,12 @@
 <
 WCSessionDelegate
 >
-@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *countLabel;
+
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceButton *headerBtn;
 @property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceTable *tableView;
-/** 所有接受对象 */
+/** 所有接收对象 */
 @property (nonatomic, strong) NSMutableArray *allReceiveObjects;
+@property WCSession *session;
 @end
 
 
@@ -38,28 +42,54 @@ WCSessionDelegate
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
     [super willActivate];
-    [WCSession defaultSession];
-    [[WCSession defaultSession] setDelegate:self];
-    [[WCSession defaultSession] activateSession];
+    self.session =[WCSession defaultSession];
+    self.session.delegate = self;
+    //* 必须激活session */
+    [self.session activateSession];
+}
+- (IBAction)removeAllBtn {
+    //* 删除 */
+    NSRange range = NSMakeRange(0, self.allReceiveObjects.count);
+    [self.tableView removeRowsAtIndexes:[NSIndexSet indexSetWithIndexesInRange:range]];
+    [self.headerBtn setTitle:@"已删除全部"];
+    [self.allReceiveObjects removeAllObjects];
+    //* 发送数据到iOS */
+    NSDictionary *infoDic = @{@"iWatch":@"iWatch已删除全部"};
+    //* 方式1 */
+    [self.session sendMessage:infoDic replyHandler:nil errorHandler:nil];
+    //* 方式2 */
+//    [self.session transferUserInfo:infoDic];
 }
 #pragma mark - WCSessionDelegate
+//* 接收数据方式1 配合 transferCurrentComplicationUserInfo */
 - (void)session:(WCSession *)session didReceiveUserInfo:(NSDictionary<NSString *,id> *)userInfo {
     HiSchool *hischool = [HiSchool new];
     hischool.avatarData = userInfo[@"avatar"];
     hischool.name = userInfo[@"name"];
     hischool.content = userInfo[@"content"];
     [self.allReceiveObjects addObject:hischool];
-    [self.countLabel setText:[NSString stringWithFormat:@"count:%i",self.allReceiveObjects.count]];
-    [self.tableView setNumberOfRows:self.allReceiveObjects.count withRowType:@"HSCell"];
-    __weak typeof(self) weakSelf = self;
-    for (NSInteger i=0; i<self.allReceiveObjects.count; i++) {
-        HiSchool *theHiSchool = self.allReceiveObjects[i];
-        [self animateWithDuration:0.8f animations:^{
-            HiSchoolCell *cell = [weakSelf.tableView rowControllerAtIndex:i];
-            [cell setCellContent:theHiSchool];
-        }];
-    }
+    [self.headerBtn setTitle:[NSString stringWithFormat:@"count:%i\n点击此处删除全部",self.allReceiveObjects.count]];
+
+    //* 新增数据 */
+    [self.tableView insertRowsAtIndexes:[NSIndexSet indexSetWithIndex:0]  withRowType:@"HSCell"];
+    HiSchoolCell *cell = [self.tableView rowControllerAtIndex:0];
+    [cell setCellContent:hischool];
 }
+
+//* 接收数据方式2 配合 sendMessage: replyHandler: errorHandler: */
+//- (void)session:(WCSession *)session didReceiveMessage:(NSDictionary<NSString *,id> *)message {
+//    HiSchool *hischool = [HiSchool new];
+//    hischool.avatarData = message[@"avatar"];
+//    hischool.name = message[@"name"];
+//    hischool.content = message[@"content"];
+//    [self.allReceiveObjects addObject:hischool];
+//    [self.headerBtn setTitle:[NSString stringWithFormat:@"count:%i\n点击此处删除全部",self.allReceiveObjects.count]];
+//    
+//    //* 新增数据 */
+//    [self.tableView insertRowsAtIndexes:[NSIndexSet indexSetWithIndex:0]  withRowType:@"HSCell"];
+//    HiSchoolCell *cell = [self.tableView rowControllerAtIndex:0];
+//    [cell setCellContent:hischool];
+//}
 - (void)didDeactivate {
     // This method is called when watch view controller is no longer visible
     [super didDeactivate];
--- IWatch_TableView/IWatch_TableView/ViewController.m
+++ IWatch_TableView/IWatch_TableView/ViewController.m
@@ -6,16 +6,24 @@
 //  Copyright © 2016年 LaiYoung. All rights reserved.
 //
 
+/** https://github.com/CYBoys/iWatch */
+
 /** The iOS app and iWatch the communication between the use of 'WCSession' */
 /** Touch the screen to send data to iWatch  */
 /** Please run on the iOS side, make sure the iphone app */
 
 #import "ViewController.h"
 #import <WatchConnectivity/WatchConnectivity.h>
+#define SCREEN_SIZE [[UIScreen mainScreen] bounds].size
 @interface ViewController ()
 <
 WCSessionDelegate
 >
+@property WCSession *session;
+@property (nonatomic, strong) UIButton *button;
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, weak) UILabel *label;
+@property (nonatomic, assign,getter=isTimerStatus) BOOL timerStatus;
 @end
 
 @implementation ViewController
@@ -23,21 +31,86 @@ WCSessionDelegate
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"HiSchool&iWatch";
+    self.session = [WCSession defaultSession];
+    self.session.delegate = self;
+    //* 必须激活session */
+    [self.session activateSession];
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 150, 40)];
+    button.center = self.view.center;
+    button.layer.cornerRadius = 10;
+    button.layer.masksToBounds = YES;
+    [button setTitle:[self dateStr] forState:UIControlStateNormal];
+    button.tintColor = [UIColor whiteColor];
+    button.backgroundColor = [UIColor redColor];
+    [button addTarget:self action:@selector(sendMessageToiWatch) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    self.button = button;
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100, 100,SCREEN_SIZE.width-(100*2) , 200)];
+    label.font = [UIFont systemFontOfSize:20];
+    label.numberOfLines = 0;
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = @"点击按钮发送信息到iWatch";
+    [self.view addSubview:label];
+    self.label = label;
+
+    self.timer =  [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(updateBtnTitle) userInfo:nil repeats:YES];
+ 
+    self.timerStatus = YES;
+}
+- (void)updateBtnTitle {
+    [self.button setTitle:[self dateStr] forState:UIControlStateNormal];
+}
+- (void)sendMessageToiWatch {
+    if (!self.isTimerStatus) {
+        //* 开始定时器 */
+        NSDate *date = [NSDate distantPast];
+        [self.timer setFireDate:date];
+        self.label.text = @"点击按钮发送信息到iWatch";
+    }
+    NSData *avatarData = UIImagePNGRepresentation([UIImage imageNamed:@"IMG_0852"]);
+    
+    NSString *contnet = [@"Hello," stringByAppendingString:[NSString stringWithFormat:@"--%@",[self dateStr]]];
+    NSDictionary *infoDic = @{@"avatar":avatarData,@"name":@"LaiYoung",@"content":contnet};
+    //* 发送数据到iWatch端 */
+    //* 方式1  */
+    [self.session transferCurrentComplicationUserInfo:infoDic];
+    //* 方式2 */
+//    [self.session sendMessage:infoDic replyHandler:nil errorHandler:nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [[WCSession defaultSession] setDelegate:self];
-    [[WCSession defaultSession] activateSession];
-    NSData *avatarData = UIImagePNGRepresentation([UIImage imageNamed:@"braceletLiked"]);
-    NSDictionary *infoDic = @{@"avatar":avatarData,@"name":@"LaiYoung",@"content":@"Hello LaiYoung"};
-    [[WCSession defaultSession] transferCurrentComplicationUserInfo:infoDic];
+- (NSString *)dateStr {
+    NSDate *date = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"HH时mm分ss秒"];
+    NSString *dateStr = [dateFormatter stringFromDate:date];
+    return dateStr;
 }
 #pragma mark - WCSessionDelegate
 - (void)session:(WCSession *)session didFinishUserInfoTransfer:(WCSessionUserInfoTransfer *)userInfoTransfer error:(NSError *)error {
     NSLog(@"%@",error);
+}
+//* 接收数据方式1 配合 transferUserInfo */
+-(void)session:(WCSession *)session didReceiveUserInfo:(NSDictionary<NSString *,id> *)userInfo {
+    NSLog(@"userinfo = %@",userInfo);
+    self.label.text = [NSString stringWithFormat:@"iWatch:%@\n%@",userInfo[@"iWatch"],[self dateStr]];
+    //* 暂停定时器 */
+    NSDate *date = [NSDate distantFuture];
+    [self.timer setFireDate:date];
+
+}
+//* 接收数据方式2 配合 sendMessage: replyHandler: errorHandler: */
+- (void)session:(WCSession *)session didReceiveMessage:(NSDictionary<NSString *,id> *)message {
+    self.label.text =[NSString stringWithFormat:@"iWatch:%@\n%@",message[@"iWatch"],[self dateStr]];
+    //* 暂停定时器 */
+    NSDate *date = [NSDate distantFuture];
+    [self.timer setFireDate:date];
+    self.timerStatus = NO;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 @end
